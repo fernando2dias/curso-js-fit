723b2d50933214e01fbe758c51fe3a1ef97092af
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-05-22 01:22:17 -0300

    Math methods

---
+++ 04-metodos-matematica.js
@@ -0,0 +1,73 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+
+//Sum
+val = num1 + num2;
+console.log(val);
+
+//Multiply
+val = num1 * num2;
+console.log(val);
+
+//Minus
+val = num1 - num2;
+console.log(val);
+
+//Division
+val = num1 / num2;
+console.log(val);
+
+//Module
+val = num1 % num2;
+console.log(val);
+
+// Math Object
+val = Math.PI;
+console.log(val);
+
+// round
+val = Math.round(2.8);
+console.log(val);
+
+// round for more
+val = Math.ceil(2.8);
+console.log(val);
+
+// round for less
+val = Math.floor(2.8);
+console.log(val);
+
+// square root
+val = Math.sqrt(64);
+console.log(val);
+
+// absolute number
+val = Math.abs(-2);
+console.log(val);
+
+// exponent number
+val = Math.pow(8,2);
+console.log(val);
+
+// minimus number
+val = Math.min(3,5,7,2,4,223,64);
+console.log(val);
+
+// maximus number
+val = Math.max(32,576,321,2,5);
+console.log(val);
+
+
+// random number
+val = Math.random();
+console.log(val);
+
+val = Math.ceil(Math.random() * 20);
+console.log(val);
+
+
+
+
