870f7ca17dc83cd5e3e22209de8fef68edb3003e
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-06-07 18:08:36 -0300

    funcoes

---
+++ app.js
@@ -0,0 +1,43 @@
+// FUNCTION DECLARATIONS
+function greet(){
+    return 'Hello';
+}
+console.log(greet());
+
+function greet2(firstName, lastName){
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+console.log(greet2('Fernando', 'Dias Motta'));
+
+
+// FUNCTION EXPRESSIONS
+
+const square = function(x){
+    return x*x;
+}
+console.log(square(6));
+
+
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSION - IIFs
+(function(){
+    console.log('IIFE Ran..');
+})();
+
+(function(name){
+    console.log('Hello ' + name);
+})('Fernando');
+
+
+// PROPERTY METHODS
+const todo = {
+    add: function(){
+        console.log('Add todo...');
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`)
+    }
+}
+
+todo.add();
+todo.edit(21);
